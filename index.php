caf056ba055022de40ddaaf2d5915a6f2a5dc79f
AakashSankhalpara <93640245+AakashSankhalpara@users.noreply.github.com>
2023-02-18 20:56:45 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Accountancy Office Homepage</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <header>
+      <div class="container">
+        <div class="logo">
+          <a href="#">Accountancy Office</a>
+        </div>
+        <nav>
+          <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <section id="hero">
+      <div class="container">
+        <h1>Welcome to Accountancy Office</h1>
+        <p>We provide top-notch accounting services to small and medium-sized businesses.</p>
+        <a href="#" class="btn">Learn More</a>
+      </div>
+    </section>
+    <section id="services">
+      <div class="container">
+        <h2>Our Services</h2>
+        <div class="service">
+          <i class="fas fa-chart-line"></i>
+          <h3>Financial Planning</h3>
+          <p>We provide expert financial planning and analysis to help you reach your goals.</p>
+        </div>
+        <div class="service">
+          <i class="fas fa-hand-holding-usd"></i>
+          <h3>Bookkeeping</h3>
+          <p>We offer comprehensive bookkeeping services to help you stay on top of your finances.</p>
+        </div>
+        <div class="service">
+          <i class="fas fa-file-invoice-dollar"></i>
+          <h3>Tax Preparation</h3>
+          <p>We help you stay compliant and minimize your tax burden with expert tax preparation services.</p>
+        </div>
+      </div>
+    </section>
+    <section id="about">
+      <div class="container">
+        <h2>About Us</h2>
+        <p>We are a team of experienced accountants dedicated to providing the highest level of service to our clients. Our mission is to help small and medium-sized businesses succeed by providing expert financial advice and services.</p>
+        <a href="#" class="btn">Meet Our Team</a>
+      </div>
+    </section>
+    <section id="testimonials">
+      <div class="container">
+        <h2>Testimonials</h2>
+        <div class="testimonial">
+          <p>"Accountancy Office has been an invaluable partner in managing our finances. Their expertise and guidance have been instrumental in helping us grow our business."</p>
+          <p class="name">John Doe, CEO</p>
+        </div>
+        <div class="testimonial">
+          <p>"I highly recommend Accountancy Office to any business looking for top-notch accounting services. They are professional, responsive, and always go the extra mile for their clients."</p>
+          <p class="name">Jane Smith, CFO</p>
+        </div>
+      </div>
+    </section>
+    <section id="contact">
+      <div class="container">
+        <h2>Contact Us</h2>
+        <form>
+          <label for="name">
